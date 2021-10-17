5b140613e6e3d6e5d1a3908d1a5adb06990faa68
krisvelikov <k.velikov@student.utwente.nl>
2021-10-17 18:49:03 +0200

    MaxSessionsPerPerson(1) + SessionExpery
    - An user could be logged on 1 device (browser) at a time
    - After 30 minutes of inactivity the session expires

--- RC/src/main/java/M5Project/RC/Security/SecurityConfig.java
+++ RC/src/main/java/M5Project/RC/Security/SecurityConfig.java
@@ -1,15 +1,8 @@
 package M5Project.RC.Security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-
-import javax.net.ssl.SSLContext;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -20,14 +13,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         httpSecurity
                 .csrf().disable()
-                .antMatcher("/**").authorizeRequests()             //endpoints that require login
-                .antMatchers("/", "/error", "/css/**","/images/**","js/**").permitAll()
+                .antMatcher("/**").authorizeRequests()    //endpoints that require login
+                .antMatchers("/", "/error", "/css/**","/images/**","js/**").permitAll() //endpoints that don't require login
                 .antMatchers("/oauth2/authorization/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login()
                     .loginPage("/")
                     .defaultSuccessUrl("/logged", true);
+
+        httpSecurity.sessionManagement()
+                .maximumSessions(1)
+                .expiredUrl("/?expired=true");
+
     }
 
 }
