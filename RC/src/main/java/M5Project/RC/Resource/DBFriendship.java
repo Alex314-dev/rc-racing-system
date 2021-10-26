04fc46d236e79664191dc09f7a800004001a9421
AleksanderP <petrov.k.alexander@gmail.com>
2021-10-26 09:24:32 +0200

    Finished friends functionality, added PLayerfriend constructor to Player

--- RC/src/main/java/M5Project/RC/Resource/DBFriendship.java
+++ RC/src/main/java/M5Project/RC/Resource/DBFriendship.java
@@ -2,6 +2,7 @@ package M5Project.RC.Resource;
 
 import M5Project.RC.model.Player;
 import M5Project.RC.model.Race;
+import org.springframework.security.core.parameters.P;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -81,10 +82,10 @@ public class DBFriendship {
 
                 requestSuccessFlag =  0;
             } else if (isFriends == false){
-                System.out.println("Already requested");
+                System.out.println("Already in request");
                 requestSuccessFlag =  2;
             } else if (isOngoingRequest == false){
-                System.out.println("Already friends");
+                System.out.println("Already in friendship");
                 requestSuccessFlag = 1;
             }
 
@@ -97,6 +98,11 @@ public class DBFriendship {
         }
     }
 
+    /**
+     * Respond to a request. AKA change the valid flag to true for the given players
+     * @param friend1
+     * @param friend2
+     */
     public static void respondToRequest(String friend1, String friend2){
         loadDriver();
         try {
@@ -119,6 +125,173 @@ public class DBFriendship {
             connection.close();
         } catch (SQLException sqle) {
             System.err.println("Error connecting: " + sqle);
+        }
+    }
+
+    /**
+     * Method to get all friends requests. If pending boolean is set,
+     * then return all pending requests, if not, return all outgoing requests.
+     * @param username the username of the current user
+     * @param pending the pending flag (decides output)
+     * @return
+     */
+    public static List<String> getRequests(String username, boolean pending) {
+        loadDriver();
+        List<String> friends = new ArrayList<>();
+        String requests = "";
+        String friend = "";
+
+        if (pending) {
+            requests = "SELECT f.friend1\n" +
+                    "FROM friendship f\n" +
+                    "WHERE f.friend2 = ?\n" +
+                    "AND f.valid = false";
+            friend = "friend1";
+        } else {
+            requests = "SELECT f.friend2\n" +
+                    "FROM friendship f\n" +
+                    "WHERE f.friend1 = ?\n" +
+                    "AND f.valid = false";
+            friend = "friend2";
+        }
+
+        try {
+            Connection connection = getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(requests);
+            statement.setString(1, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+                friends.add(resultSet.getString(friend));
+            }
+
+            statement.close();
+            connection.close();
+            return friends;
+        } catch (SQLException sqle) {
+            System.err.println("Error connecting: " + sqle);
+            return null;
+        }
+    }
+
+    /**
+     * Method to delete a friendship between the current player and a specified user
+     * @param friend friend to unfriend
+     */
+    public static void deleteFriend(String username, String friend){
+        loadDriver();
+        try {
+            Connection connection = getConnection();
+
+            String deleteFriendship = "DELETE FROM friendship\n" +
+                    "WHERE (friend1 = ?\n" +
+                    "AND friend2 = ?)\n" +
+                    "OR (\n" +
+                    "friend1 = ?\n" +
+                    "AND friend2 = ?)";
+
+            PreparedStatement statement = connection.prepareStatement(deleteFriendship);
+            statement.setString(1, username);
+            statement.setString(2, friend);
+            statement.setString(3, friend);
+            statement.setString(4, username);
+            statement.execute();
+
+        } catch (SQLException sqle) {
+            System.err.println("Error connecting: " + sqle);
+        }
+    }
+
+    /**
+     * Method to return the current friends of a user with their wins and losses
+     * @param username this player username
+     * @return a list of players with wins and losses (friends)
+     */
+    public static List<Player> getFriendsWinsLosses(String username) {
+        loadDriver();
+        List<Player> friends = new ArrayList<>();
+        try {
+            Connection connection = getConnection();
+            String friendsQuery = "SELECT p.username as friends\n" +
+                    "FROM player p, friendship f\n" +
+                    "WHERE (f.friend1 = ?\n" +
+                    "AND f.friend2 = p.username " +
+                    "AND f.valid = true)\n" +
+                    "OR (f.friend2 = ?\n" +
+                    "AND f.friend1 = p.username " +
+                    "AND f.valid = true)\n";
+
+            PreparedStatement statement = connection.prepareStatement(friendsQuery);
+            statement.setString(1, username);
+            statement.setString(2, username);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()) {
+                String friendUsername = resultSet.getString("friends");
+                Player newFriend = new Player(friendUsername, 0, 0);
+                friends.add(newFriend);
+            }
+
+            //win, loss
+            String friendsWins1 = "SELECT jsonb_agg(json_build_object('win', f.friend2win, 'loss', f.friend1win) ) as score, " +
+                    "f.friend2win as wins, f.friend1win as losses\n" +
+                    "FROM friendship f\n" +
+                    "WHERE f.friend1 = ?\n" +
+                    "AND f.friend2 = ?" +
+                    "GROUP BY wins, losses";
+
+            //win, loss
+            String friendsWins2 = "SELECT f.friend1win as wins, f.friend2win as losses\n" +
+                    "FROM friendship f\n" +
+                    "WHERE f.friend2 = ?\n" +
+                    "AND f.friend1 = ?";
+
+
+            for (Player friend: friends) {
+                statement = connection.prepareStatement(friendsWins1);
+                statement.setString(1, username);
+                statement.setString(2, friend.getUsername());
+                ResultSet resultSet1 = statement.executeQuery();
+
+                System.out.println(friend.getUsername());
+
+                int wins = 0;
+                int losses = 0;
+                boolean otherQuery = true;
+
+                while (resultSet1.next()) {
+                    String check = resultSet1.getString("score");
+                    if (check != null) {
+                        wins = resultSet1.getInt("wins");
+                        losses = resultSet1.getInt("losses");
+                        otherQuery = false;
+                    }
+                }
+
+                if (otherQuery) {
+                    statement = connection.prepareStatement(friendsWins2);
+                    statement.setString(1, username);
+                    statement.setString(2, friend.getUsername());
+                    ResultSet resultSet2 = statement.executeQuery();
+
+                    while (resultSet2.next()) {
+                        wins = resultSet2.getInt("wins");
+                        losses = resultSet2.getInt("losses");
+                    }
+                }
+
+                friend.setWins(wins);
+                friend.setLosses(losses);
+            }
+
+            connection.close();
+            statement.close();
+
+            return friends;
+        } catch (SQLException sqle) {
+            System.err.println("Error connecting: " + sqle);
+            return null;
         }
     }
 
@@ -151,6 +324,13 @@ public class DBFriendship {
     }
 
     public static void main(String[] args) {
-        DBFriendship.respondToRequest("LoopingLaurens", "KaganTheMan");
+        //DBFriendship.sendFriendRequest("AlexP", "LoopingLaurens");
+        //DBFriendship.deleteFriend("LoopingLaurens", "AlexP");
+
+        List<Player> friends = DBFriendship.getFriendsWinsLosses("KaganTheMan");
+        for (Player friend: friends) {
+            System.out.println(friend.toStringFriend());
+        }
+
     }
 }
