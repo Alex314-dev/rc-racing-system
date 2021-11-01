46ec5a76e81c0757134132e359405364f469d976
s2596415 <l.neta@student.utwente.nl>
2021-11-01 16:33:25 +0100

    start worling on accepting a challenge

--- RC/src/main/java/M5Project/RC/Controller/RESTAPIController.java
+++ RC/src/main/java/M5Project/RC/Controller/RESTAPIController.java
@@ -99,4 +99,9 @@ public class RESTAPIController {
         }
         return -1;
     }
+
+    @GetMapping("/rest/acceptChallenge")
+    public float acceptChallenge(Principal principal) {
+        ;
+    }
 }
