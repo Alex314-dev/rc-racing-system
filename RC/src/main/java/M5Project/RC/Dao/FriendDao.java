2a36acd1c77639667fbc4ef2a508f48ccfd28c9a
s2596415 <l.neta@student.utwente.nl>
2021-11-03 11:52:57 +0100

    Add Friends DAO for friendships

--- RC/src/main/java/M5Project/RC/Dao/FriendDao.java
+++ RC/src/main/java/M5Project/RC/Dao/FriendDao.java
@@ -1,7 +1,35 @@
 package M5Project.RC.Dao;
 
+import M5Project.RC.Resource.DBFriendship;
+import M5Project.RC.model.Player;
+
+import java.util.List;
+
 public enum FriendDao {
     instance;
 
 
+    public int sendFriendRequest(String sender, String friendToAdd) {
+        return DBFriendship.sendFriendRequest(sender, friendToAdd);
+    }
+
+    public int acceptFriendRequest(String current, String sender) {
+        return DBFriendship.respondToRequest(current, sender);
+    }
+
+    public List<String> getPendingRequests(String username) {
+        return DBFriendship.getRequests(username, true);
+    }
+
+    public List<String> getSentRequests(String username) {
+        return DBFriendship.getRequests(username, false);
+    }
+
+    public List<Player> getFriendsWinsLosses(String username) {
+        return DBFriendship.getFriendsWinsLosses(username);
+    }
+
+    public int deleteFriend(String current, String friendToDelete) {
+        return DBFriendship.deleteFriend(current, friendToDelete);
+    }
 }
